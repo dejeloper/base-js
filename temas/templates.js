55a129100711c9e229508d1ae6d4505052b25950
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-08-29 23:20:01 -0500

    Add Template

---
+++ temas/templates.js
@@ -0,0 +1,14 @@
+const nombre = "Jhonatan";
+const apellido = "Guerrero";
+
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+console.log(`Resultado: ${1 + 1}`);
+
+function getSaludo(nombre) {
+  return `Hola ${nombre}`;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)}`);
