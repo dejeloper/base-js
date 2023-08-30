6f4be49717a292aecb9c8d0d8691ccea0b7b1aef
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-08-29 23:43:26 -0500

    Add funciones 1

---
+++ temas/funciones.js
@@ -0,0 +1,45 @@
+// function saludo( nombre){
+// 	return `Hola ${nombre}`
+// }
+
+const saludo = (nombre) => `Hola ${nombre}`;
+
+const nombre = "Juan";
+
+console.log(saludo(nombre));
+
+const saludo2 = (nombre = "Maria") => {
+  return `Hola ${nombre}`;
+};
+
+console.log(saludo2());
+
+const getUser = () => {
+  return {
+    uid: "ABC123",
+    username: "dejeloper 1",
+  };
+};
+
+console.log(getUser());
+
+const getUser2 = () => ({ uid: "ABC123", username: "dejeloper 2" });
+
+console.log(getUser2());
+
+const heroes = [
+  {
+    id: 1,
+    name: "Batman",
+  },
+  {
+    id: 2,
+    name: "Superman",
+  },
+];
+
+const existe = heroes.some((heroe) => heroe.id === 1);
+console.log(existe);
+
+const { name, id } = heroes.find((heroe) => heroe.id === 2);
+console.log(name);
