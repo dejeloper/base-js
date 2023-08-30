dec7761f5e2b6687b7241ace073cf99a255e3478
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-08-29 23:16:39 -0500

    Add Variables

---
+++ temas/variables.js
@@ -0,0 +1,16 @@
+const nombre = "Juan";
+const apellido = "De la torre";
+
+console.log(nombre, apellido);
+
+if (true) {
+  const nombre = "Pedro";
+  console.log(nombre);
+}
+
+if (true) {
+  let nombre = "Pablo";
+  console.log(nombre);
+}
+
+console.log(nombre);
